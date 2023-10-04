f871fb79960576d400b315eb7368405b5bc41a3b
Hearues Zueke <hziko314@gmail.com>
2023-10-04 07:42:33 +0200

    simple clicking in the tiles possible

--- simple_gui_click.py
+++ simple_gui_click.py
@@ -3,76 +3,123 @@ import sys
 import time
 
 import numpy as np
+import tkinter as tk
 
 from PIL import ImageTk, Image
-
-import tkinter as tk
+from tkinter import messagebox
 
 # Define useful parameters
 WINDOW_WIDTH = 960
 WINDOW_HEIGHT = 540
-
+BACKGROUND_COLOR = "#1a7e56"
 
 class SimpleClickGUICanvas:
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Simple-Click-GUI-Canvas")
-        self.canvas = tk.Canvas(self.window, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, bd=0)
+        self.root = tk.Tk()
+        self.root.title("Simple-Click-GUI-Canvas")
+
+        self.canvas = tk.Canvas(
+            self.root,
+            width=WINDOW_WIDTH, height=WINDOW_HEIGHT,
+            bd=0, insertwidth=0, insertborderwidth=0, selectborderwidth=0, highlightthickness=0, # remove the border from canvas!
+        )
         self.canvas.pack()
+        self.canvas.configure(bg=BACKGROUND_COLOR)
         # Input from user in form of clicks and keyboard
-        self.window.bind("<Key>", self.key_input)
-        self.window.bind("<Button-1>", self.mouse_input)
+        self.root.bind("<Key>", self.key_input)
+        self.root.bind("<Button-1>", self.mouse_input)
         self.play_again()
-        self.begin = False
+        self.is_exit = False
+
+        self.root.protocol("WM_DELETE_WINDOW", self.on_exit)
 
 
-    def initialize_board(self):
-        rows = 6
-        cols = 6
+    def on_exit(self):
+        """When you click to exit, this function is called"""
+        if messagebox.askyesno("Exit", "Do you want to quit the application?"):
+            self.is_exit = True
 
-        boxes_x = 100
-        boxes_y = 80
-        boxes_w = 50
-        boxes_h = 50
 
-        board_line_width = 5
-        board_x = boxes_x - board_line_width
-        board_y = boxes_y - board_line_width
+    def initialize_board_variables(self):
+        self.rows = 6
+        self.cols = 6
 
-        boxes_line_width = 3
+        self.boxes_x_min = 100
+        self.boxes_y_min = 80
+        self.boxes_w = 50
+        self.boxes_h = 50
 
-        l_color_fill = ["#779355", "#ebebd0"]
-        l_color_outer = ["#3c571d", "#94957c"]
+        self.board_line_width = 5
+        self.board_x = self.boxes_x_min - self.board_line_width
+        self.board_y = self.boxes_y_min - self.board_line_width
 
-        for row in range(0, rows):
-            for col in range(0, cols):
-                idx_color = (row + col) % len(l_color_fill)
+        self.boxes_line_width = 3
+
+        self.l_color_fill = ["#779355", "#ebebd0"]
+        self.l_color_outer = ["#3c571d", "#94957c"]
+
+        self.boxes_x_max = self.boxes_x_min + self.boxes_w * self.cols
+        self.boxes_y_max = self.boxes_y_min + self.boxes_h * self.rows
+
+
+    def draw_board(self):
+        for row in range(0, self.rows):
+            for col in range(0, self.cols):
+                idx_color = (row + col) % len(self.l_color_fill)
                 self.own_rectangle(
-                    x=boxes_x+col*boxes_w, y=boxes_y+row*boxes_h,
-                    w=boxes_w, h=boxes_h, line_width=boxes_line_width,
-                    fill=l_color_fill[idx_color], outer=l_color_outer[idx_color],
+                    x=self.boxes_x_min+col*self.boxes_w, y=self.boxes_y_min+row*self.boxes_h,
+                    w=self.boxes_w, h=self.boxes_h, line_width=self.boxes_line_width,
+                    fill=self.l_color_fill[idx_color], outer=self.l_color_outer[idx_color],
                 )
 
         self.own_rectangle_outer_only(
-            x=board_x, y=board_y,
-            w=boxes_w*cols+2*board_line_width, h=boxes_h*rows+2*board_line_width,
-            line_width=board_line_width,
+            x=self.board_x, y=self.board_y,
+            w=self.boxes_w*self.cols+2*self.board_line_width, h=self.boxes_h*self.rows+2*self.board_line_width,
+            line_width=self.board_line_width,
             outer="#000000",
         )
 
 
     def play_again(self):
         self.canvas.delete("all")
-        self.initialize_board()
+        self.initialize_board_variables()
+        self.draw_board()
     
 
     def mainloop(self):
-        while True:
-            self.window.update()
+        while not self.is_exit:
+            self.root.update()
 
 
+    # TODO: add a board matrix and check if a tile is clicked already for the beginning
     def mouse_input(self, event):
-        pass
+        pos_x = event.x
+        pos_y = event.y
+        print(f"clicked at: {{pos_x: {pos_x}, pos_y: {pos_y}}}")
+
+        # calculate the center of one border tile/box
+        # if it is ouside the tile, return none
+        if not (
+            (pos_x >= self.boxes_x_min) and (pos_x < self.boxes_x_max) and
+            (pos_y >= self.boxes_y_min) and (pos_y < self.boxes_y_max)
+        ):
+            print("- Missed the tiles!")
+            return
+
+        tile_x = (pos_x - self.boxes_x_min) // self.boxes_w
+        tile_y = (pos_y - self.boxes_y_min) // self.boxes_h
+
+        pos_tile_x_mid = self.boxes_x_min + self.boxes_w // 2 + tile_x * self.boxes_w
+        pos_tile_y_mid = self.boxes_y_min + self.boxes_h // 2 + tile_y * self.boxes_h
+
+        radius = (self.boxes_w * 0.6) // 2
+        x1 = pos_tile_x_mid - radius
+        y1 = pos_tile_y_mid - radius
+        x2 = pos_tile_x_mid + radius
+        y2 = pos_tile_y_mid + radius
+
+        color = '#' + ''.join([f'{val:02x}' for val in np.random.randint(0x40, 0xC0, (3, ))])
+        self.canvas.create_oval(x1, y1, x2, y2, outline="black", fill=color, width=2)
         
 
     def key_input(self, event):
